e5bad2563c6334e8ba1048bbec86ab271dd5e261
lazerpig <astefaniak270@gmail.com>
2025-11-12 22:25:14 -0800

    can move left and right

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-
+from player import Player
 
 
 def main():
@@ -10,14 +10,22 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
+    Player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
+
     while 1 > 0:
         
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        Player_1.update(dt)
         screen.fill("black")
+        Player_1.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
